99094d451a15b122812029e5d5b75c0fb2530161
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 17:36:03 -0700

    List assignment error

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import random
 
 def main():
     total = 0
+    i = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    while (total <= 100):
+        numbers [i] = random.randint(0, 100)
+        total = sum(numbers)
+
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
+
     return numbers, total
 
 
